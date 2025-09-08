b0f750d8f511a90ee4689024644c9df06f79fc64
Amir <skmdaamir29@gmail.com>
2025-09-08 18:29:35 +0530

    change

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const session = require("express-session");
-const blogRoutes = require("./api/routes/blogRouters");
+
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const adminRoutes = require("./api/routes/admin");
 const bodyParser = require("body-parser");
 const statesRoute = require("./api/routes/states");
@@ -17,8 +17,12 @@ const galleryRoute = require("./api/routes/uploadImageVideos");
 const reviewRoutes = require("./api/routes/reviews");
 const careerRoutes = require("./api/routes/careerRoutes");
 const conditionRoutes = require("./api/routes/conditionsRoutes");
+const blogRoutes = require("./api/routes/blogRouters");
 
 
+
+app.use(cors());
+app.use(express.json());
 // Session Configuration
 app.use(
   session({
@@ -60,11 +64,8 @@ app.use((req, res, next) => {
   // methods: ["GET", "POST", "PUT", "DELETE"],
   // credentials: true,
 // }
-app.use(cors());
-app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "..", "..", "..","..", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use("/api", blogRoutes);
-app.use(bodyParser.json());
 app.use("/api/states", statesRoute);
 app.use("/api/cities", citiesRoute);
 app.use("/api/treatment", treatmentRoute);
