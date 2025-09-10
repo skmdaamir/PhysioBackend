8a58960cf91a2ddb93a21ce544e7d7b3f76250cc
Amir <skmdaamir29@gmail.com>
2025-09-10 20:11:07 +0530

    login api change

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -4,14 +4,12 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const db = require("../../dbConnection"); // Import the shared connection pool
 
-// console.log(process.env.JWT_SECRET);
 const SECRET_KEY = process.env.JWT_SECRET;
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Execute query using the connection pool from dbConnection.js
     const [rows] = await db.execute("SELECT * FROM users WHERE email = ?", [
       email,
     ]);
@@ -20,15 +18,22 @@ router.post("/login", async (req, res) => {
     if (!user || user.role !== "admin") {
       return res.status(401).json({ message: "Unauthorized" });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
+    // Generate token with 15m expiry
     const token = jwt.sign({ id: user.id, role: user.role }, SECRET_KEY, {
       expiresIn: "15m",
     });
-    res.json({ token });
+
+    // Decode expiry time
+    const decoded = jwt.decode(token);
+    const exp = decoded.exp; // Unix timestamp (seconds)
+
+    res.json({ token, exp });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
